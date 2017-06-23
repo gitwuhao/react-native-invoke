3bf9d0cadfb647a113bafbb1765482bf3b900fa9
gitwuhao <gitwuhao@users.noreply.github.com>
2017-06-23 10:29:42 +0800

    Update dev.js

--- dev.js
+++ dev.js
@@ -5,7 +5,9 @@ function RNIDev(handle) {
 	var createNativeInvoke = native.createNativeInvoke;
 	var nativeInvoke;
 	console.warn("RNI is web debug mode.");
-	window.postMessage = window.TESTINNERRNI.postMessage = function (message) {
+
+	//将Web端的消息发送给Native端
+	window.postMessage = window.INNERRNI.postMessage = function (message) {
 		var data;
 		try {
 			data = JSON.parse(message);
@@ -20,10 +22,15 @@ function RNIDev(handle) {
 		});
 	};
 
+	//创建nativeInvoke对象，传递一个将Native消息发送给web端的函数
 	nativeInvoke = createNativeInvoke(function (message) {
-		window.TESTINNERRNI.onMessageHandle({
+		//将Native端的消息发送给Web端
+		window.INNERRNI.onMessageHandle({
 			data: message
 		});
+
+		//React Native可以通过webviewComponent.postMessage将消息回传给web端，
+		//web端通过document.addEventListener('message', RNI.onMessageHandle.bind(RNI))接收消息;
 	});
 	window['originalPostMessage'] = function () {
 
